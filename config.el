80e58ada1532f666aa5f12c638dbca0bd82897a6
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-28 21:27:42 +0530

    feat: add org journal and global auto revert config

--- config.el
+++ config.el
@@ -7,12 +7,22 @@
 ; NOTE setting relative line number
 (setq display-line-numbers-type 'relative)
 
+(global-auto-revert-mode 1)
+(setq global-auto-revert-non-file-buffers t)
+
 ; NOTE Default Org Directory
 (setq org-directory "~/org/")
 ; NOTE Default Note File
 (setq org-default-notes-file (concat org-directory "/refile.org"))
 ; NOTE Default Org Roam Directory
 (setq org-roam-directory "~/notebook/")
+
+; NOTE Setting up org journal directory
+(setq org-journal-dir "~/org/journal/"
+      org-journal-date-prefix "* "
+      org-journal-time-prefix "** "
+      org-journal-date-format "%B %d, %Y (%A) "
+      org-journal-file-format "%Y-%m-%d.org")
 
 ; NOTE Setting vterm keybinding
 (map! :leader
