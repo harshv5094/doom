8733b8b6ba6615ec98638a7a31dcbeb4c4fb54ef
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-30 23:23:03 +0530

    feat: improving emacs installation script

--- install.sh
+++ install.sh
@@ -1,30 +1,56 @@
 #!/usr/bin/env bash
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+CYAN=$(tput setaf 6)
+RESET=$(tput sgr0)
+
 function have() {
     command -v "$1" &>/dev/null
 }
 
-printf "%b\n" "Installing Emacs"
+function cmdCheck() {
+    if [ $? -eq 0 ]; then
+        printf "%b\n" "${GREEN}Success!${RESET}"
+    else
+        printf "%b\n" "${RED}Fail!!${RESET}"
+    fi
+}
+
+printf "%b\n" "${CYAN}Installing Emacs.${RESET}"
 if have pacman; then
     sudo pacman -S emacs ttf-jetbrains-mono-nerd libvterm
+    cmdCheck
 fi
 
+printf "%b\n" "${CYAN}Checking for old emacs directory (if exist).${RESET}"
 if [ -e "$HOME/.config/emacs" ]; then
-    printf "%b\n" "Removing Old Emacs Directory"
+    printf "%b\n" "${YELLOW}Removing Old Emacs Directory.${RESET}"
     rm -rf "$HOME/.config/emacs"
+    cmdCheck
 elif [ -e "$HOME/.emacs.d" ]; then
-    printf "%b\n" "Removing Old Emacs Directory"
+    printf "%b\n" "${YELLOW}Removing Old Emacs Directory.${RESET}"
     rm -rf "$HOME/.emacs.d"
+    cmdCheck
 elif [ -e "$HOME/.doom.d" ]; then
-    printf "%b\n" "Removing Old Doom Emacs Directory"
+    printf "%b\n" "${YELLOW}Removing Old Doom Emacs Directory.${RESET}"
     rm -rf "$HOME/.doom.d"
+    cmdCheck
 else
-    printf "%b\n" "Old Config Directories are not present in this config"
+    printf "%b\n" "${GREEN}Old Config Directories are not present in this system.${GREEN}"
 fi
 
 # Clonning doom emacs repo
-printf "%b\n" "Setting Up Doom Emacs"
-git clone --depth 1 "https://github.com/doomemacs/doomemacs" ~/.config/emacs
+printf "%b\n" "${YELLOW}Setting Up Doom Emacs.${RESET}"
+git clone --depth 1 "https://github.com/doomemacs/doomemacs." ~/.config/emacs
+cmdCheck
 
+# Starting Installation Process
 ~/.config/emacs/bin/doom install
 ~/.config/emacs/bin/doom sync
+
+# Checking if last commands execution returns 0 as exit code
+if [ $? -eq 0 ]; then
+    printf "%b\n" "${GREEN} Installation process is finished.${RESET}"
+fi
