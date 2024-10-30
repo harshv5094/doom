a3882ae1414ed974805c89697c36868dc329a7d7
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-30 15:55:45 +0530

    refactor: simplifying installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-printf "%b\n" "Installing emacs config"
+function have() {
+  command -v "$1" &>/dev/null
+}
+
+printf "%b\n" "Installing Emacs"
+if have pacman; then
+  sudo pacman -S emacs ttf-jetbrains-mono-nerd
+fi
+
+# Clonning doom emacs repo
+printf "%b\n" "Setting Up Doom Emacs"
 git clone --depth 1 "https://github.com/doomemacs/doomemacs" ~/.config/emacs
+
 ~/.config/emacs/bin/doom install
 ~/.config/emacs/bin/doom sync
-
-printf "%b\n" "Setting up Emacs Fonts"
-if command -v pacman &>/dev/null; then
-  sudo paru -S ttf-ubuntu-font-family ttf-jetbrains-mono-nerd
-fi
