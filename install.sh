ea8dbc41ace41a01cb2ec90f3cdc4d816d719575
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-11-12 18:25:57 +0530

    chore: removing readme org file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
@@ -7,21 +7,21 @@ CYAN=$(tput setaf 6)
 RESET=$(tput sgr0)
 
 function have() {
-    command -v "$1" &>/dev/null
+  command -v "$1" &>/dev/null
 }
 
 function cmdCheck() {
-    if [ $? -eq 0 ]; then
-        printf "%b\n" "${GREEN}Success!${RESET}"
-    else
-        printf "%b\n" "${RED}Fail!!${RESET}"
-    fi
+  if [ $? -eq 0 ]; then
+    printf "%b\n" "${GREEN}Success!${RESET}"
+  else
+    printf "%b\n" "${RED}Fail!!${RESET}"
+  fi
 }
 
 printf "%b\n" "${CYAN}Installing Emacs.${RESET}"
 if have pacman; then
-    sudo pacman -S emacs ttf-jetbrains-mono-nerd ttf-firacode-nerd libvterm
-    cmdCheck
+  sudo pacman -S emacs ttf-jetbrains-mono-nerd ttf-firacode-nerd libvterm
+  cmdCheck
 fi
 
 # Turning on emacs daemon
@@ -29,19 +29,19 @@ systemctl --user enable --now emacs
 
 printf "%b\n" "${CYAN}Checking for old emacs directory (if exist).${RESET}"
 if [ -e "$HOME/.config/emacs" ]; then
-    printf "%b\n" "${YELLOW}Removing Old Emacs Directory.${RESET}"
-    rm -rf "$HOME/.config/emacs"
-    cmdCheck
+  printf "%b\n" "${YELLOW}Removing Old Emacs Directory.${RESET}"
+  rm -rf "$HOME/.config/emacs"
+  cmdCheck
 elif [ -e "$HOME/.emacs.d" ]; then
-    printf "%b\n" "${YELLOW}Removing Old Emacs Directory.${RESET}"
-    rm -rf "$HOME/.emacs.d"
-    cmdCheck
+  printf "%b\n" "${YELLOW}Removing Old Emacs Directory.${RESET}"
+  rm -rf "$HOME/.emacs.d"
+  cmdCheck
 elif [ -e "$HOME/.doom.d" ]; then
-    printf "%b\n" "${YELLOW}Removing Old Doom Emacs Directory.${RESET}"
-    rm -rf "$HOME/.doom.d"
-    cmdCheck
+  printf "%b\n" "${YELLOW}Removing Old Doom Emacs Directory.${RESET}"
+  rm -rf "$HOME/.doom.d"
+  cmdCheck
 else
-    printf "%b\n" "${GREEN}Old Config Directories are not present in this system.${GREEN}"
+  printf "%b\n" "${GREEN}Old Config Directories are not present in this system.${GREEN}"
 fi
 
 # Clonning doom emacs repo
@@ -55,5 +55,5 @@ printf "%b\n" "${YELLOW}Installing Doom Emacs.${RESET}"
 
 # Checking if last commands execution returns 0 as exit code
 if [ $? -eq 0 ]; then
-    printf "%b\n" "${GREEN} Installation process is finished.${RESET}"
+  printf "%b\n" "${GREEN} Installation process is finished.${RESET}"
 fi
