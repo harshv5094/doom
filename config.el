0b5a5475466e3353a57d2dee2f0121320832a7c1
Harsh Vyapari (@harshv5094) <harshv5094@gmail.com>
2024-12-21 12:36:18 +0530

    feat: adding markdown header size

--- config.el
+++ config.el
@@ -31,6 +31,15 @@
       doom-modeline-persp-name t                 ;; adds perspective name to modeline
       doom-modeline-persp-icon t)                ;; adds folder icon next to persp name
 
+(custom-set-faces
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.7))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.6))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.5))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.4))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.3))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.2)))))
+
 (after! org
   (setq org-agenda-files '("~/org/agenda.org")))
 
